1bb0c27813f6da2eb5f40877b0036911a4ed63fb
Ismael Ali <ismaelali78@outlook.com>
2022-08-26 08:58:10 +0100

    rendered player on canvas

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useInterval } from "../../useInterval";
 
 export default function Dashboard() {
   //Character Movement Directions
@@ -10,20 +11,23 @@ export default function Dashboard() {
   };
 
   const USER_START = [
-    [8, 7],
-    [8, 8],
+    [200, 200],
+    [200, 200],
   ];
 
   //Direction of moving character, initially set to move upwards
   const [dir, setDir] = useState([0, -1]);
   const [user, setUser] = useState(USER_START);
+  const [keyPress, setKeyPress] = useState(false);
 
   //Gets value of canvas element inside of the canvasRef variable
   const canvasRef = useRef();
 
   //Moves character
   const moveCharacter = ({ keyCode }) => {
-    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
+    console.log("PRESS KEY");
+    setDir(DIRECTIONS[keyCode]);
+    setKeyPress(true);
   };
 
   //Loads character
@@ -38,7 +42,95 @@ export default function Dashboard() {
     // };
 
     ctx.fillStyle = "blue";
-    ctx.fillRect(100, 100, 20, 20);
+    user.forEach(([x, y]) => ctx.fillRect(x, y, 20, 20));
+  }, [user]);
+
+  // Char
+  var char = {
+    x: 50,
+    y: 50,
+    width: 20,
+    height: 42,
+    spriteX: 0,
+    spriteY: 0,
+    speed: 150,
+    edgeRegion: 50,
+    moving: false,
+    animateTime: 2,
+    animateCur: 0,
+    animatePos: Array(0, 42, 84, 42, 0, 128, 170, 128),
+  };
+  var charReady = false;
+  var charImg = new Image();
+  charImg.onload = function () {
+    charReady = true;
+  };
+
+  charImg.src =
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/15388/knightd25b8b7e.png";
+
+  const gameLoop = () => {
+    const userCopy = JSON.parse(JSON.stringify(user));
+    const newUserHead = [userCopy[0][0] + dir[0], userCopy[0][1] + dir[1]];
+    userCopy.unshift(newUserHead);
+    userCopy.pop();
+    setUser(userCopy);
+  };
+
+  //   useInterval(() => gameLoop(), 1000);
+
+  // Background
+  var background = { x: 0, y: 0, width: 512, height: 480 };
+  var backgroundReady = false;
+  var backgroundImg = new Image();
+  backgroundImg.onload = function () {
+    backgroundReady = true;
+  };
+  backgroundImg.src =
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/15388/background.png";
+
+  // Render Function
+  var render = function () {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, 512, 480);
+
+    if (char.moving) {
+      if (char.animateTime > 0) {
+        char.animateTime--;
+      } else {
+        if (char.animateCur + 1 < char.animatePos.length) {
+          char.animateCur++;
+          char.spriteY = char.animatePos[char.animateCur];
+        } else {
+          char.animateCur = 0;
+          char.spriteY = char.animatePos[0];
+        }
+        char.animateTime = 2;
+      }
+    }
+
+    if (backgroundReady) {
+      ctx.drawImage(backgroundImg, background.x, background.y);
+    }
+
+    if (charReady) {
+      ctx.drawImage(
+        charImg,
+        char.spriteX,
+        char.spriteY,
+        char.width,
+        char.height,
+        char.x,
+        char.y,
+        char.width,
+        char.height
+      );
+    }
+  };
+
+  useEffect(() => {
+    render();
   }, []);
 
   return (
