e8f02ff4d27d7799603db5b9aa0d2a3a99995cad
eheath30 <elliot.heath@btinternet.com>
2022-08-26 13:02:43 +0100

    login and registration

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
 import { Link, useNavigate } from "react-router-dom";
 
+export default function SignIn({ setWhichModal }) {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
 
-export default function SignIn({setWhichModal}) {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    return null
+  };
+
 
   function goToOther() {
-    setWhichModal('register')
+    setWhichModal("register");
   }
-
 
   return (
     <section>
-      <div>SignIn</div>
+      <h1>Welcome!</h1>
+      <p>Log in with your username and password.</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          label="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        ></input>
+        <br/>
+        <input
+          type="password"
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        ></input>
+      </form>
+
       <p>
         Don't have an account? <button onClick={goToOther}>Register.</button>
       </p>
