3d2abe7eb6f0dab90669cbe630f4a8c5dd117c06
eheath30 <elliot.heath@btinternet.com>
2022-08-27 20:20:18 +0100

    shop modal created

--- src/components/ShopWindow/index.js
+++ src/components/ShopWindow/index.js
@@ -1,13 +1,53 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
 export default function ShopWindow() {
   return (
     <section className="rpgui-content">
-
       <div className="rpgui-container framed-golden-2 shop-window">
-        shop window
+        <div class="row">
+        <div class="col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center">
+            <div>
+            <div class="rpgui-icon potion-blue"></div>
+              <div class="rpgui-icon potion-red"></div>
+              <div class="rpgui-icon potion-green"></div>
+
+            </div>
+            <div class="product-content">
+              <h3 class="title">
+                <a href="#">Buy</a>
+              </h3>
+
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center">
+            <div>
+            <div class="rpgui-icon potion-green"></div>
+              <div class="rpgui-icon potion-red"></div>
+
+              <div class="rpgui-icon potion-blue"></div>
+            </div>
+            <div class="product-content">
+              <h3 class="title">
+                <a href="#">Trade</a>
+              </h3>
+
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center">
+            <div>
+              <div class="rpgui-icon potion-red"></div>
+              <div class="rpgui-icon potion-green"></div>
+              <div class="rpgui-icon potion-blue"></div>
+            </div>
+            <div class="product-content">
+              <h3 class="title">
+                <a href="#">Create a listing</a>
+              </h3>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-  )
+  );
 }
