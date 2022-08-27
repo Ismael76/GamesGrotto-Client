9018e3508e76b4b927c098a70e1ffbdaf9b3dd14
eheath30 <elliot.heath@btinternet.com>
2022-08-27 19:46:11 +0100

    styling

--- src/components/ShopWindow/index.js
+++ src/components/ShopWindow/index.js
@@ -1,1 +1,13 @@
-import React from "react";
+import React from 'react'
+import './styles.css'
+
+export default function ShopWindow() {
+  return (
+    <section className="rpgui-content">
+
+      <div className="rpgui-container framed-golden-2 shop-window">
+        shop window
+      </div>
+    </section>
+  )
+}
