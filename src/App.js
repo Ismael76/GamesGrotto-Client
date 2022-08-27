f33f0e36cfa6568971aef342d06c864e69fef2c1
Ismael Ali <ismaelali78@outlook.com>
2022-08-27 17:34:52 +0100

    private routes implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { Splash, Shop, Home } from "./pages";
 import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
+import PrivateRoutes from "./utils/PrivateRoute";
 
 function App() {
   return (
     <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/home" element={<Home />} />
+      </Route>
       <Route path="/" element={<Splash />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
