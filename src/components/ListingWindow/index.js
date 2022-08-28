79ab0382bad1a533e0143f487fae5a95bd46e797
eheath30 <elliot.heath@btinternet.com>
2022-08-28 19:53:27 +0100

    modal

--- src/components/ListingWindow/index.js
+++ src/components/ListingWindow/index.js
@@ -1,9 +1,32 @@
 import React, { useEffect, useContext, useState, useNavigate } from "react";
 import "./styles.css"
+import { GameContext } from "../../ContextProvider";
+
+
+import Modal from "react-modal";
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(64, 223, 219,0.3)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    color: "black",
+    opacity: "0.92",
+  },
+};
+Modal.setAppElement("#root");
+
 
 export default function ListingWindow(listingType) {
   const [gameType, setGameType] = useState("Video Game")
-
+  const [section, modal] = useContext(GameContext);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const dummyData = [
     {name:"Call of Duty", description:"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.", price:12, location:"London"},
@@ -18,17 +41,34 @@ export default function ListingWindow(listingType) {
   console.log("The gametype is: ",gameType)
 
 
-  const showListing = () => 
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+
+  const showListing = () =>
+
     dummyData.map((val, key) => (
         <tr key={key}>
+
           <td className="p-3">{val.name}</td>
           <td className="p-3">{val.description}</td>
           <td className="p-3">£{val.price}</td>
           <td className="p-3">{val.location}</td>
+
+          <button className="rpgui-button mt-5" onClick={openModal}>More</button>
+          <hr className="golden"/>
+
         </tr>
+
       )
     )
-  
+
 
   return (
     <section className="rpgui-content">
@@ -51,6 +91,20 @@ export default function ListingWindow(listingType) {
           {/* <Listings listingType={listingType}/> */}
         </div>
       </div>
+      <Modal
+            className="rpgui-content splash-modal-position"
+            ref={modal}
+            closeTimeoutMS={500}
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Authentication modal"
+          >
+             <div className="rpgui-container framed d-flex flex-column text-center">
+        <button className="position-absolute" onClick={closeModal}>X</button>
+        <h1>Header</h1>
+        </div>
+          </Modal>
     </section>
   );
 }
