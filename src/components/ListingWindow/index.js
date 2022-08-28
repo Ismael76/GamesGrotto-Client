4d55cec36f9b904589997123aa71494c9203ac59
Ismael Ali <ismaelali78@outlook.com>
2022-08-28 14:55:13 +0100

    displaying components for trade or buy in the shop

---
+++ src/components/ListingWindow/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ListingWindow() {
+  return (
+    <section className="rpgui-content">
+      <div className="rpgui-container framed-golden-2 shop-window"></div>
+    </section>
+  );
+}
--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useContext } from "react";
-import './styles.css'
+import React, { useEffect, useContext, useState } from "react";
+import "./styles.css";
 import { GameContext } from "../../ContextProvider";
+import { ListingWindow } from "../../components";
 
 export default function Shop() {
   const [section, modal, homeSection] = useContext(GameContext);
+  const [showListing, setShowListing] = useState(false);
 
   function unfade(element) {
     var op = 0.1; // initial opacity
@@ -18,62 +20,75 @@ export default function Shop() {
     }, 40);
   }
 
+  const handleClick = () => {
+    setShowListing(true);
+  };
+
   useEffect(() => {
     unfade(homeSection.current);
   }, []);
 
   return (
-  <section ref={homeSection} className='shop bg-dark'>
-    <section className="rpgui-content">
-      <div className="rpgui-container framed-golden-2 shop-window">
-        <div className="d-flex flex-row-reverse justify-content-between pb-2 pl-2">
-          <a href="#" className="">
-            <div class="rpgui-icon exclamation flex-item"></div>
-          </a>
-          <a href="#" className="">
-            <div class="rpgui-container flex-item">Back</div>
-          </a>
-        </div>
-        <div class="d-flex row justify-content-center pb-5 w-auto">
-          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
-            <div>
-              <div class="rpgui-icon chest-open"></div>
+    <section ref={homeSection} className="shop bg-dark">
+      {showListing ? (
+        <ListingWindow />
+      ) : (
+        <section className="rpgui-content">
+          <div className="rpgui-container framed-golden-2 shop-window">
+            <div className="d-flex flex-row-reverse justify-content-between pb-2 pl-2">
+              <a href="#" className="">
+                <div class="rpgui-icon exclamation flex-item"></div>
+              </a>
+              <a href="#" className="">
+                <div class="rpgui-container flex-item">Back</div>
+              </a>
             </div>
-            <div class="product-content">
-              <h3 class="title mx-md-5 py-1">
-                <button className="rpgui-button d-block m-auto  ">
-                  <a href="#">Buy</a>
-                </button>
-              </h3>
+            <div class="d-flex row justify-content-center pb-5 w-auto">
+              <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+                <div>
+                  <div class="rpgui-icon chest-open"></div>
+                </div>
+                <div class="product-content">
+                  <h3 class="title mx-md-5 py-1">
+                    <button
+                      className="rpgui-button d-block m-auto"
+                      onClick={handleClick}
+                    >
+                      <a href="#">Buy</a>
+                    </button>
+                  </h3>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+                <div>
+                  <div class="rpgui-icon shield"></div>
+                </div>
+                <div class="product-content">
+                  <h3 class="title mx-md-5 py-1">
+                    <button className="rpgui-button d-block m-auto  ">
+                      <a href="#" onClick={handleClick}>
+                        Trade
+                      </a>
+                    </button>
+                  </h3>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+                <div>
+                  <div class="rpgui-icon cash"></div>
+                </div>
+                <div class="product-content">
+                  <h3 class="title mx-md-5 py-1">
+                    <button className="rpgui-button d-block m-auto  ">
+                      <a href="#">Sell</a>
+                    </button>
+                  </h3>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
-            <div>
-              <div class="rpgui-icon shield"></div>
-            </div>
-            <div class="product-content">
-              <h3 class="title mx-md-5 py-1">
-                <button className="rpgui-button d-block m-auto  ">
-                  <a href="#">Trade</a>
-                </button>
-              </h3>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
-            <div>
-              <div class="rpgui-icon cash"></div>
-            </div>
-            <div class="product-content">
-              <h3 class="title mx-md-5 py-1">
-                <button className="rpgui-button d-block m-auto  ">
-                  <a href="#">Sell</a>
-                </button>
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
+        </section>
+      )}
     </section>
-  </section>
-    )
+  );
 }
