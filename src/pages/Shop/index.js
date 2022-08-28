a811a6e52e9f2494ec37f7f7a985c704e0fc6845
eheath30 <elliot.heath@btinternet.com>
2022-08-28 14:41:40 +0100

    .

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useContext } from "react";
 import './styles.css'
-import { ShopWindow } from '../../components'
 import { GameContext } from "../../ContextProvider";
 
 export default function Shop() {
@@ -25,7 +24,56 @@ export default function Shop() {
 
   return (
   <section ref={homeSection} className='shop bg-dark'>
-    <ShopWindow />
+    <section className="rpgui-content">
+      <div className="rpgui-container framed-golden-2 shop-window">
+        <div className="d-flex flex-row-reverse justify-content-between pb-2 pl-2">
+          <a href="#" className="">
+            <div class="rpgui-icon exclamation flex-item"></div>
+          </a>
+          <a href="#" className="">
+            <div class="rpgui-container flex-item">Back</div>
+          </a>
+        </div>
+        <div class="d-flex row justify-content-center pb-5 w-auto">
+          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+            <div>
+              <div class="rpgui-icon chest-open"></div>
+            </div>
+            <div class="product-content">
+              <h3 class="title mx-md-5 py-1">
+                <button className="rpgui-button d-block m-auto  ">
+                  <a href="#">Buy</a>
+                </button>
+              </h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+            <div>
+              <div class="rpgui-icon shield"></div>
+            </div>
+            <div class="product-content">
+              <h3 class="title mx-md-5 py-1">
+                <button className="rpgui-button d-block m-auto  ">
+                  <a href="#">Trade</a>
+                </button>
+              </h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 d-flex flex-column pt-3 justify-content-center align-items-center rpgui-container framed-grey shadow mx-3">
+            <div>
+              <div class="rpgui-icon cash"></div>
+            </div>
+            <div class="product-content">
+              <h3 class="title mx-md-5 py-1">
+                <button className="rpgui-button d-block m-auto  ">
+                  <a href="#">Sell</a>
+                </button>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </section>
     )
 }
