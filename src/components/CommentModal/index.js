0d72d614e4ae155084439cc3a88c2c69a2c64cdf
Rameez <rameez0khawaja@gmail.com>
2022-08-29 12:55:11 +0100

    forum posts and comments

---
+++ src/components/CommentModal/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios"
+
+
+export default function CommentModal({ post, closeModal }) {
+
+    const [commentData, setCommentData] = useState([]);
+
+    useEffect(async () => {
+        try {
+            const url = `http://localhost:5000/comments/${post.id}`
+            const data = await axios.get(url)
+            setCommentData(data.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    const currentPost = () =>
+        <>
+            <div>
+                <h3 className="p-3">{post.title}</h3>
+                <p className="p-3">{post.text}</p>
+                <p className="p-3 text-center">
+                    Posted by {post.username} {/*on {item.date} */}
+                </p>
+            </div>
+            <hr className="golden" />
+        </>
+
+    const renderComments = () =>
+        commentData.map((item) => (
+            <>
+                <div>
+                    <p className="p-3">{item.text}</p>
+                    <p className="p-3 text-center">
+                        Commented by {item.username} {/*on {item.date} */}
+                    </p>
+                </div>
+                <hr className="golden" />
+            </>
+        ))
+
+    return (
+        <section>
+            <a href="#"><div class="rpgui-container position-absolute" onClick={closeModal}>X</div></a>
+            {currentPost()}
+            {commentData.length !=0 && renderComments()}
+        </section>
+    );
+}
--- src/components/ForumWindow/index.js
+++ src/components/ForumWindow/index.js
@@ -1,7 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./styles.css";
 import { GameContext } from "../../ContextProvider";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import Modal from "react-modal";
+import CommentModal from "../CommentModal";
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    color: "black",
+    opacity: "1",
+  },
+};
+Modal.setAppElement("#root");
 
 export default function ForumWindow() {
   const [
@@ -15,87 +35,45 @@ export default function ForumWindow() {
   ] = useContext(GameContext);
 
   const navigate = useNavigate();
+  const [postData, setPostData] = useState([])
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [post, setPost] = useState();
 
-  const dummyData = [
-    {
-      id: 1,
-      heading: "Hello Gamers",
-      content:
-        " I tried to get some Pikachus to get on a bus the other day. It was tough, in the end I had to poke em on.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 2,
-      heading: "Hello World",
-      content: "Hi guys",
-      username: "Billy",
-      date: "12/03/2022",
-    },
-    {
-      id: 3,
-      heading: "How do I access the shop?",
-      content:
-        "Playing Tetris has taught me an extremely valuable life lesson. If you fit in, you're going to disappear.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 4,
-      heading: "Hello Gamers",
-      content:
-        " Garbage men are the best team mates. They're used to carrying trash.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 5,
-      heading: "Hello Gamers",
-      content:
-        "My friend was so angry when he lost at the video game we were playing, that he smashed up his keyboard. He definitely lost Control.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 6,
-      heading: "Hello Gamers",
-      content:
-        "A botanist was playing Minecraft, and had to become a math teacher. He needed to calculate the cubic root.  ",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 7,
-      heading: "Hello Gamers",
-      content:
-        "I started making fun of the official Minecraft twitter account last week. I was gutted when they blocked me.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-    {
-      id: 8,
-      heading: "Hello Gamers",
-      content: "I asked a French man if he played video games. He said Wii.",
-      username: "Bob",
-      date: "12/03/2022",
-    },
-  ];
+  useEffect(async () =>{
+    try {
+      const url = "http://localhost:5000/posts"
+      const data = await axios.get(url)
+      setPostData(data.data)
+    } catch(error) {
+      console.log(error)
+    }
+  }, [])
 
+  const openModal = (post) => {
+    setPost(post)
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+  
   const renderListing = () =>
-    dummyData.map((item) => (
+    postData.map((item) => (
       <>
-        <li key={item.id}>
-          <h3 className="p-3">{item.heading}</h3>
-          <p className="p-3">{item.content}</p>
+        <div key={item.id} onClick={() =>openModal(item)}>
+          <h3 className="p-3">{item.title}</h3>
+          <p className="p-3">{item.text}</p>
           <p className="p-3 text-center">
-            Posted by {item.username} on {item.date}
+             Posted by {item.username} {/*on {item.date} */}
           </p>
-        </li>
+        </div>
         <hr className="golden" />
       </>
     ));
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     console.log("SUBMIT");
   };
 
@@ -107,25 +85,38 @@ export default function ForumWindow() {
 
   return (
     <section className="rpgui-content">
+      {!modalIsOpen && <>
       <div className="rpgui-container framed-golden forum-search">
         <a href="#" onClick={handleBack}>
           <div className="rpgui-container flex-item">Back</div>
         </a>
         <form onSubmit={handleSubmit} className="d-flex mt-4">
-          <input className="m-1" value="Search game here"></input>
+          <input className="m-1" placeholder="Search title here"></input>
           <button className="m-1">Submit</button>
         </form>
       </div>
 
       <div className="rpgui-container framed-golden forum-window">
-        {/*
-        <h1>Take me far away from the mucky muck</h1>
-        <hr className="golden"/>
-        <h1>To a castle made of clouds</h1> */}
         <h1>Forum Board</h1>
         <hr className="golden" />
-        <ul>{renderListing()}</ul>
+        {postData.length ==0 && <h1>No Posts Available</h1>}
+        {postData.length !=0 && <ul>{renderListing()}</ul>}
       </div>
+      </>}
+      {modalIsOpen && <>
+      <Modal
+        className="rpgui-content splash-modal-position"
+        ref={modal}
+        closeTimeoutMS={500}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Authentication modal"
+      ></Modal>
+      <div className="rpgui-container framed d-flex flex-column text-center listing-modal">
+        <CommentModal post={post} closeModal={closeModal} />
+      </div>
+      </>}
     </section>
   );
 }
