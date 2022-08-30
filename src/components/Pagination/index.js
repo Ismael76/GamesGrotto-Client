1f23803b5581d8ed635e80eb93ae9cc70e21065a
Ismael Ali <ismaelali78@outlook.com>
2022-08-30 11:03:34 +0100

    fixed issue with pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Pagination({ postsPerPage, totalPosts, paginate }) {
   const pageNumbers = [];
 
-  for (let i = 1; i < Math.ceil(totalPosts / postsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
 
