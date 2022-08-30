9bbf7849a3ba6fe097253958ece4e636ed327560
Ismael Ali <ismaelali78@outlook.com>
2022-08-30 17:12:41 +0100

    added ability for users to view and delete their own listings

--- src/components/ListingWindow/index.js
+++ src/components/ListingWindow/index.js
@@ -181,7 +181,6 @@ export default function ListingWindow({ listingType, setShowListing }) {
                 ? renderListingSale()
                 : renderListingTrade()}
             </table>
-
             <Pagination
               postsPerPage={postsPerPage}
               totalPosts={
--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,4 @@ export { default as ForumWindow } from "./ForumWindow";
 export { default as ListingModal } from "./ListingModal";
 export { default as ContactModal } from "./ContactModal";
 export { default as GameModal } from "./GameModal";
-export { default as InfoModal } from "./InfoModal";
 export { default as Pagination } from "./Pagination";
