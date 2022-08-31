56d89c49a6481468968ecab8ce36e18e42a4723c
Ismael Ali <ismaelali78@outlook.com>
2022-08-31 12:58:24 +0100

    changed style of leaderboard modal

--- src/components/GameModal/index.js
+++ src/components/GameModal/index.js
@@ -147,7 +147,7 @@ export default function GameModal({
           style={customStyles}
           contentLabel="Authentication modal"
         >
-          <div className="rpgui-container framed-golden-2 d-flex flex-column text-center p-4 score-modal">
+          <div className="rpgui-container framed-golden d-flex flex-column text-center p-4 score-modal">
             <button className="position-absolute" onClick={closeModal}>
               X
             </button>
